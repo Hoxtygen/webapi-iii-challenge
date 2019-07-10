2cf9546cfa23c75a839c218d02c948444cbc907e
Hoxtygen <hoxtygen@live.com>
2019-07-10 13:19:53 +0100

    Get all posts

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,8 +1,16 @@
-
+const express = require('express');
+const Model = require("./postDb.js");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-
+router.get('/', async(req, res) => {
+    try {
+        const posts = await Model.get();
+        res.status(200).json(posts);
+    } catch (error) {
+        res.status(500).json({
+            errorMessage: error
+        })
+    }
 });
 
 router.get('/:id', (req, res) => {
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
-
 const server = express();
+const postRouter = require('./posts/postRouter.js')
+
+
+server.use('/api/posts', postRouter)
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
