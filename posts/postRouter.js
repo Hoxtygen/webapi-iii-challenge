18fe6044c6ec5309e9df0534ae27d163125c04ba
Hoxtygen <hoxtygen@live.com>
2019-07-10 13:44:34 +0100

    Remove post from the database

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -33,8 +33,24 @@ router.get('/:id', async(req, res) => {
     }
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', async(req, res) => {
+   const id = parseInt(req.params.id, 10);
+   try {
+       const post = await Model.remove(id);
+       if (post) {
+        return res.status(200).json({
+            message: 'post successfully deleted'
+        });
+       } else {
+        return res.status(404).json({
+            message: "The post with the specified ID does not exist."
+        })
+       }
+   } catch (error) {
+    res.status(500).json({
+        error: "The post could not be removed"
+    })
+   }
 });
 
 router.put('/:id', (req, res) => {
