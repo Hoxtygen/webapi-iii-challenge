ea4514c1131c42116426fed62cefeafb8ef722d2
Hoxtygen <hoxtygen@live.com>
2019-07-10 15:35:59 +0100

    Remove a user

--- users/userRouter.js
+++ users/userRouter.js
@@ -47,8 +47,24 @@ router.get('/:id/posts', (req, res) => {
 
 });
 
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id', async(req, res) => {
+    const id = parseInt(req.params.id, 10);
+   try {
+       const user = await usersModel.remove(id);
+       if (user) {
+        return res.status(200).json({
+            message: 'user successfully deleted'
+        });
+       } else {
+        return res.status(404).json({
+            message: "The user with the specified ID does not exist."
+        })
+       }
+   } catch (error) {
+    res.status(500).json({
+        error: "The user could not be removed"
+    })
+   }
 });
 
 router.put('/:id', (req, res) => {
